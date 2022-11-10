99748326c11530987a871f983a19438e60f16dc2
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-11-10 17:09:13 +0100

    add support for native signInWithGoogleRedirect, add useEffect to create user using modern React standards

--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -1,18 +1,37 @@
-import { signInWithGooglePopup, createUserDocumentFromAuth } from '../../utils/firebase/firebase.utils'
+import { useEffect } from 'react'
+import { getRedirectResult } from 'firebase/auth'
+
+import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
 
 
 const SignIn = () => {
 
+    useEffect(() => {
+        const fetchData = async () => {
+        const response = await getRedirectResult(auth)
+        if (response) {
+            const userDocRef = await createUserDocumentFromAuth(response.user)
+        }
+        }
+        fetchData()
+        .catch(console.error)
+    }, [])
     const logGoogleUser = async () => {
         const {user} = await signInWithGooglePopup()
         const userDocRef = await createUserDocumentFromAuth(user)
     }
+
+    // const logGoogleRedirectUser = async () => {
+    //     const {user} = await signInWithGoogleRedirect()
+    //     console.log({user});
+    // }
 
 
     return (
         <div>
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>Sign in with Google pop up</button>
+            <button onClick={signInWithGoogleRedirect}>Sign in with Google redirect</button>
         </div>
 
     )
