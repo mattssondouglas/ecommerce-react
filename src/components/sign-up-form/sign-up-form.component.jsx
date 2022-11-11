c7b926ab15f43c09a4b45d8f8f8ed12c6e160e14
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-11-11 16:39:45 +0100

    add signupform component to signin page

--- src/components/sign-up-form/sign-up-form.component.jsx
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -2,20 +2,22 @@ const SignUpForm = () => {
     return (
         <div>
             <h1>Sign up with email and password</h1>
-            <form action="">
+            <form onSubmit={()=>{}}>
                 <label htmlFor="">Username</label>
-                <input type="text" />
+                <input type="text" required placeholder="Enter your username"/>
 
                 <label htmlFor="">Email</label>
-                <input type="email" />
+                <input type="email" required placeholder="Enter your email"/>
 
                 <label htmlFor="">Password</label>
-                <input type="password" />
+                <input type="password" required placeholder="Enter your password"/>
 
                 <label htmlFor="">Confirm Password</label>
-                <input type="password" />
+                <input type="password" required placeholder="Confirm your password"/>
+                <button type='submit'>Sign up</button>
 
             </form>
         </div>
     )
 }
+export default SignUpForm
--- src/routes/sign-in/sign-in.component.jsx
+++ src/routes/sign-in/sign-in.component.jsx
@@ -2,7 +2,7 @@ import { useEffect } from 'react'
 import { getRedirectResult } from 'firebase/auth'
 
 import { auth, signInWithGooglePopup, createUserDocumentFromAuth, signInWithGoogleRedirect } from '../../utils/firebase/firebase.utils'
-
+import SignUpForm from '../../components/sign-up-form/sign-up-form.component'
 
 const SignIn = () => {
 
@@ -32,6 +32,7 @@ const SignIn = () => {
             <h1>Sign in page</h1>
             <button onClick={logGoogleUser}>Sign in with Google pop up</button>
             <button onClick={signInWithGoogleRedirect}>Sign in with Google redirect</button>
+            <SignUpForm />
         </div>
 
     )
