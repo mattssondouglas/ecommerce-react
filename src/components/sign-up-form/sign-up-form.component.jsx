f14a1a0da304b78b12982852b3cf8cc6566742e7
Douglas <61967200+mattssondouglas@users.noreply.github.com>
2022-11-11 16:32:40 +0100

    add sign up component

---
+++ src/components/sign-up-form/sign-up-form.component.jsx
@@ -0,0 +1,21 @@
+const SignUpForm = () => {
+    return (
+        <div>
+            <h1>Sign up with email and password</h1>
+            <form action="">
+                <label htmlFor="">Username</label>
+                <input type="text" />
+
+                <label htmlFor="">Email</label>
+                <input type="email" />
+
+                <label htmlFor="">Password</label>
+                <input type="password" />
+
+                <label htmlFor="">Confirm Password</label>
+                <input type="password" />
+
+            </form>
+        </div>
+    )
+}
